9a071d961d209a21cb6fd8b2e89ef8412bf24404
aluno cmc <alunocmc@umc.br>
2026-08-10 10:53:49 -0300

    Aula3

---
+++ Aula3/ConstrutoresMetodos/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        filme filme = new filme("Homem Aranha", "Ação",
+                "Dan Slott", 115, 35,50 );
+
+        System.out.println(filme.toString());
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite a quantidade de ingressos desejados: ");
+        int ingressos = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Olá, " + nome + ", a sua compra ficou no valor de R$ "
+                 + filme.compraIngresso(ingressos));
+
+        filme.decrementaIngresso(ingressos);
+
+        sc.close();
+    }
+}
