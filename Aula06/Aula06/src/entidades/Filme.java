618cc2cb5ec8d4defd7bfa7e86614054704d56a4
aluno cmc <alunocmc@umc.br>
2026-08-26 10:44:51 -0300

    Aula02

--- Aula06/Aula06/src/entidades/Filme.java
+++ Aula06/Aula06/src/entidades/Filme.java
@@ -1,4 +1,60 @@
 package entidades;
 
 public class Filme {
+    private String titulo;
+    private String genero;
+    private int classificacao;
+    private int duracao;
+
+    //Construtores
+    public Filme(String titulo, String genero, int classificacao, int duracao) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.classificacao = classificacao;
+        this.duracao = duracao;
+    }
+
+
+    //Getters e Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(int classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "titulo='" + titulo + '\'' +
+                ", genero='" + genero + '\'' +
+                ", classificacao=" + classificacao +
+                ", duracao=" + duracao +
+                '}';
+    }
 }
